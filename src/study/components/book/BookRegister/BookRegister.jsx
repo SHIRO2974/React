2a6ea041ac9b89a0c094ddd230300a082acba7b2
SHIRO2974 <wogus2974@gmail.com>
2025-01-18 04:52:54 +0900

    React 복습자료

--- src/study/components/book/BookRegister/BookRegister.jsx
+++ src/study/components/book/BookRegister/BookRegister.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import "./style.css";
 
-function BookRegister({ bookList, setBookList }) {
+function BookRegister({ bookList, setBookList }) {  // registerInputValue에 입력된 데이터를 bookList 배열에 추가한다
 
-    const [ registerInputValue, setRegisterInputValue ] = useState({
+    const [ registerInputValue, setRegisterInputValue ] = useState({    // 사용자의 입력값을 저장할 초기 상태 객체 생성
             bookName: "",
             author: "",
             publisher: "",
         });
     
-        const handleRegisterInputOnChange = (e) => {
+        const handleRegisterInputOnChange = (e) => {    // 사용자가 입력한 데이터를 상태에 저장한다
             setRegisterInputValue({
                 ...registerInputValue,
                 [e.target.name]: e.target.value,
             });
         }
     
-        const handleRegisterButtonOnClick = () => {
+        const handleRegisterButtonOnClick = () => { // 등록 버튼 클릭 시, 입력된 데이터를 bookList 상태에 추가한다
             setBookList([
                 ...bookList,
                 registerInputValue,
@@ -24,7 +24,7 @@ function BookRegister({ bookList, setBookList }) {
     
             alert("등록 완료.");
     
-            setRegisterInputValue({
+            setRegisterInputValue({ // 등록 완료 후 입력값을 초기화한다
                 bookName: "",
                 author: "",
                 publisher: "",
--- src/study/components/book/BookSearch/BookSearch.jsx
+++ src/study/components/book/BookSearch/BookSearch.jsx
@@ -1,61 +1,36 @@
-import "./style.css";
 import React, { useState } from 'react';
+import "./style.css";
+import BookSearchItems from '../BookSearchItems/BookSearchItems';
 
-function BookSearchItems({ bookList, setBookTableList }) {
-
-    const [ searchValue, setSearchValue ] = useState({
-        select: "bookName",
-        text: "",
-    });
-
-    const searchOptions = [
-        {
-            id: 1,
-            label: "도서명",
-            value: "bookName",
-        },
-        {
-            id: 2,
-            label: "저자명",
-            value: "author",
-        },
-        {
-            id: 3,
-            label: "출판사",
-            value: "publisher",
-        },
-    ];
-
-    const handleSearchValueOnChange = (e) => {
-        setSearchValue({
-            ...searchValue,
-            [e.target.name]: e.target.value,
-        });
-    }
-
-    const handleSearchButtonOnClick = () => {
-
-        if(!searchValue.text.trim()) {
-            setBookTableList(bookList);
-            return;
-        }
-
-        console.log(searchValue.select);
-        const foundBooks = bookList.filter(book => book[searchValue.select].includes(searchValue.text));
-        setBookTableList(foundBooks);
-    }
+function BookSearch({ bookList }) { // bookList를 참조해 찾고자하는 데이터를 
+    const [ bookTableList, setBookTableList ] = useState([]);
 
     return (
-        <div className='search-items'>
-            <select name="select" value={searchValue.select} onChange={handleSearchValueOnChange}>
-                {
-                    searchOptions.map(option => <option key={option.id} value={option.value}>{option.label}</option>)
-                }
-            </select>
-            <input type="text" name='text' value={searchValue.text} onChange={handleSearchValueOnChange} />
-            <button onClick={handleSearchButtonOnClick}>검색</button>
+        <div>
+            <h1>도서정보 조회</h1>
+            <BookSearchItems bookList={bookList} setBookTableList={setBookTableList} />
+            <table className='book-table'>
+                <thead>
+                    <tr>
+                        <th>도서명</th>
+                        <th>저자명</th>
+                        <th>출판사</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        bookTableList.map(book => 
+                            <tr>
+                                <td>{book.bookName}</td>
+                                <td>{book.author}</td>
+                                <td>{book.publisher}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
 
-export default BookSearchItems;
+export default BookSearch;
